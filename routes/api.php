6e7e06815d5c543977e56789b51133586b3609ba
Nkeoualionel <mbembasachat9@gmail.com>
2025-03-25 11:54:20 +0100

    feat: api-category

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use L5Swagger\Http\Controllers\SwaggerController;
@@ -27,6 +28,9 @@ Route::post('register', [AuthController::class, 'register'])->middleware('thrott
 Route::post('login', [AuthController::class, 'login'])->middleware('throttle:5,1');
 Route::post('check-email', [AuthController::class, 'checkEmail'])->middleware('throttle:5,1');
 Route::post('change-password', [AuthController::class, 'changePassword'])->middleware('throttle:5,1');
+
+Route::get('categories', [CategoryController::class, 'index'])->middleware('throttle:5,1');
+Route::get('/categories/{id}', [CategoryController::class, 'show'])->middleware('throttle:5,1');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
